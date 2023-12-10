88e5025bdd4570ff902064fb613cb4449afe2b0c
p4pvn <91900183+p4pvn@users.noreply.github.com>
2023-12-10 16:50:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 region     = "us-east-1"
-access_key = "AKIAS5VQFMWEPAI2XTN2"
+access_key = "enter-access-key-here"
 secret_key = "enter-your-secret-key-here"         # this user should have permission to create resources
 }
 
