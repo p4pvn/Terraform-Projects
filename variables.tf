2e7e845c2e8f7480ebcb8befad4341f92c9e9734
p4pvn <91900183+p4pvn@users.noreply.github.com>
2023-12-17 16:32:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "instance"{
+     type = string
+     default = "t2.micro"
+     sensitive = true
+}
 
+# Default type is string
+variable "ami"{
+     default = "ami-0230bd60aa48260c6"
+}
+
+# Declaring list for multiple values
+variable "instancetype"{
+    type = list
+    default = ["t2.micro","t2.large"]
+}
