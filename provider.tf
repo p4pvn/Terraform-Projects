41367dbad23346385e87a3a8a910c25cd2d16ae6
p4pvn <91900183+p4pvn@users.noreply.github.com>
2024-12-21 12:52:30 +0530

    Created provider.tf
    created an aws provider terraform file

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region     = "us-east-1"
+access_key = "enter-access-key-here"
+secret_key = "enter-your-secret-key-here"         # this user should have permission to create resources
+}
